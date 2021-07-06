fb1f4903a231de50451ff19b7a4d2126fb10f8c7
cyan <namid224y@gmail.com>
2021-07-06 09:29:18 +0900

    audit fix

--- services/fb_firestore.js
+++ services/fb_firestore.js
@@ -1,7 +1,7 @@
 // firestore reference (https://firebase.google.com/docs/reference/js/firebase.firestore.html?authuser=0)
 // firestore quickstart (https://firebase.google.com/docs/firestore/quickstart?authuser=0)
 
-import * as firebase from 'firebase/app'
+import firebase from 'firebase/app'
 import 'firebase/firestore'
 import PortfolioBuilder from '~/scripts/types/builder/PortfolioBuilder'
 
